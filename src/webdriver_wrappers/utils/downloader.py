5d2391e89188a53e73e1bedea457ae775b50cab7
Eldar MSI <kursus1010@gmail.com>
2024-11-10 14:02:02 +0100

    Implemented aarch64 webdriver downloader and minor refactor

--- src/webdriver_wrappers/utils/downloader.py
+++ src/webdriver_wrappers/utils/downloader.py
@@ -4,3 +4,6 @@ from webdriver_wrappers.utils.versions import get_chromium_version
 
 
 PATH_BIN_CHROMEDRIVER_DIR = Path(__file__).resolve().parent.parent / "_bin" / "chrome"
+
+def aarch64_webdriver_downloader() -> None:
+    print(get_chromium_version())
